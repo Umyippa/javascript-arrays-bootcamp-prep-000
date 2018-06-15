7b7852629de8bd8c2ec241fc990bdc54f39d3fea
Umyippa <sywhitehouse@gmail.com>
2018-06-15 19:29:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,6 +6,6 @@ function addElementToBeginningOfArray(chocolateBars, candyString) {
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
-  chocolateBars.unshift(candyString)
-  return
+  chocolateBars.unshift(candyString);
+  return 
 }
