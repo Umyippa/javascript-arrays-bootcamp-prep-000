5db9d672ff6b7b0c9f013d81e7fb3206b1382c72
Umyippa <sywhitehouse@gmail.com>
2018-06-15 19:09:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars) {
+  return [chocolateBars, ...chocolateBars];
+}
+
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(){
+  
+}
+
+function destructivelyAddElementToEndOfArray()
