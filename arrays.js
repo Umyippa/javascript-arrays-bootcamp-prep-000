c4fe7f8d1a8bf62da25e519271123fa14911191a
Umyippa <sywhitehouse@gmail.com>
2018-06-15 20:22:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,5 +33,6 @@ function removeElementFromBeginningOfArray(chocolateBars) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
-  return chocolateBars.pop();
+  chocolateBars.pop();
+  return chocolateBars;
 }
